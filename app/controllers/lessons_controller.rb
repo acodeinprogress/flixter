f9e0f68f73f292eb2b508cd5cfdb3151e7db4efe
Sharon Siegel <sjsieg830@gmail.com>
2015-09-23 03:09:18 +0000

    Require enrollment to access lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,15 @@
 class LessonsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :require_enrolled_for_current_lesson
+
   def show
+  end
+
+  def require_enrolled_for_current_lesson
+    if !current_user.enrolled_in?(current_lesson.section.course)
+      redirect_to course_path(current_lesson.section.course), :alert => 'You must enroll in this course before you can access it'
+    end
+
   end
 
   helper_method :current_lesson
